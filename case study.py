349786d8d081b17887a054f5df587dc99256be9e
sc20jp <94668168+sc20jp@users.noreply.github.com>
2021-11-19 08:15:55 +0000

    Add files via upload

---
+++ case study.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_excel('case.xlsx')
+value = df['newCasesByPublishDate'].values[160:1000]
+print(len(value))
+x = []
+y = []
+seq = 3
+for i in range(len(value)-seq-1):
+    x.append(value[i:i+seq])
+    y.append(value[i+seq])
+plt.plot(value[3:], label='True Value')
+plt.title('new cases (England)')
+plt.xlabel('Day')
+plt.ylabel('New Cases')
+plt.show()
